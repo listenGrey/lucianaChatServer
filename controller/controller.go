e5793b379dd459118c648b922065832719c198ae
listenGrey <tinghui561@outlook.com>
2024-05-14 19:47:16 +0800

    减少协程数

--- controller/controller.go
+++ controller/controller.go
@@ -6,44 +6,34 @@ import (
 	"lucianaChatServer/logic"
 )
 
-func NewChat(errCh chan<- error) {
+func New(errCh chan<- error) {
 	fmt.Println("new chat 服务正在运行")
+	fmt.Println("send qa 服务正在运行")
 	for {
 		// 出现错误则发送到通道
 		if err := logic.NewChat(conf.KafkaServerAddress); err != nil {
 			errCh <- err
 			return
 		}
+		// 出现错误则发送到通道
+		if err := logic.SendQA(conf.KafkaServerAddress); err != nil {
+			errCh <- err
+			return
+		}
 	}
 }
 
-func RenameChat(errCh chan<- error) {
+func Modify(errCh chan<- error) {
 	fmt.Println("rename 服务正在运行")
+	fmt.Println("delete 服务正在运行")
 	for {
 		// 出现错误则发送到通道
 		if err := logic.RenameChat(conf.KafkaServerAddress); err != nil {
 			errCh <- err
 			return
 		}
-	}
-}
-
-func DeleteChat(errCh chan<- error) {
-	fmt.Println("delete 服务正在运行")
-	for {
 		// 出现错误则发送到通道
 		if err := logic.DeleteChat(conf.KafkaServerAddress); err != nil {
-			errCh <- err
-			return
-		}
-	}
-}
-
-func SendQA(errCh chan<- error) {
-	fmt.Println("send qa 服务正在运行")
-	for {
-		// 出现错误则发送到通道
-		if err := logic.SendQA(conf.KafkaServerAddress); err != nil {
 			errCh <- err
 			return
 		}
--- main.go
+++ main.go
@@ -10,26 +10,20 @@ func main() {
 	errCh1 := make(chan error)
 	errCh2 := make(chan error)
 	errCh3 := make(chan error)
-	errCh4 := make(chan error)
-	errCh5 := make(chan error)
 
-	go controller.NewChat(errCh1)
-	go controller.RenameChat(errCh2)
-	go controller.DeleteChat(errCh3)
-	go controller.SendQA(errCh4)
-	go controller.GrpcService(errCh5)
+	go controller.New(errCh1)
+	go controller.Modify(errCh2)
+	go controller.GrpcService(errCh3)
 
 	for {
 		select {
 		case err := <-errCh1:
 			fmt.Printf("新建对话挂掉了, %s\n", err)
+			fmt.Printf("发送问题挂掉了, %s\n", err)
 		case err := <-errCh2:
 			fmt.Printf("对话重命名挂掉了, %s\n", err)
-		case err := <-errCh3:
 			fmt.Printf("删除对话挂掉了, %s\n", err)
-		case err := <-errCh4:
-			fmt.Printf("发送问题挂掉了, %s\n", err)
-		case err := <-errCh5:
+		case err := <-errCh3:
 			fmt.Printf("gRpc 服务挂掉了, %s \n", err)
 		}
 	}
